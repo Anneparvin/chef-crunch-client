33b4a46f4a92cac340cfba48ba7ab6c1bd9e0f0a
Anneparvin <108586988+Anneparvin@users.noreply.github.com>
2023-05-26 08:28:12 +0600

    add6

--- src/Components/Providers/AuthProvider.jsx
+++ src/Components/Providers/AuthProvider.jsx
@@ -1,10 +1,86 @@
-import React from 'react';
+import React, { createContext, useEffect, useState } from 'react';
+import app from '../../firebase/firebase.config';
+import {createUserWithEmailAndPassword, getAuth, GithubAuthProvider, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updatePassword, updateProfile} from 'firebase/auth';
 
-const AuthProvider = () => {
+
+export const AuthContext = createContext();
+const auth = getAuth(app);
+
+
+const googleProvider = new GoogleAuthProvider();
+const githubProvider = new GithubAuthProvider();
+
+const AuthProvider = ({children}) => {
+    const [user, setUser] = usestate(null);
+    const [loading, setloading] = useState(true);
+
+    // create user 
+    const createUser = (email, password) => {
+        setloading(true);
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+    // user sign in 
+    const signIn = (email, password) => {
+        setloading(true);
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+
+    // update user profile
+    const updateUserProfileData = (name, photo) => {
+        return updateProfile(auth.currentUser, {displayName: name, photoURL: photo});
+    }
+
+    // google sign in
+    const googleSignIn = () => {
+        setloading(true)
+        return signInWithPopup(auth, googleProvider);
+    }
+
+    // github sign in
+    const githubSignin = () => {
+        setloading(true)
+        return signInWithPopup(auth, githubProvider);
+    }
+
+    // user sign out
+    const logOut = () => {
+        return signOut(auth)
+    }
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, 
+            (currentUser) => {
+                console.log('inside auth state changed', currentUser);
+                setloading(false);
+                setUser(currentUser)
+            });
+            return () => {
+                unsubscribe();
+            }
+    },[])
+
+    // update password
+    const currentUser = auth.currentUser;
+    const updateNewPassword = password => {
+        return updatePassword(currentUser, password)
+    };
+
+    const authInfo = {
+        createUser,
+        user,
+        loading,
+        googleSignIn,
+        githubSignin,
+        signIn,
+        logOut,
+        updateUserProfileData,
+        updateNewPassword
+    }
     return (
-        <div>
-            
-        </div>
+        <AuthContext.Provider value = {authInfo}>
+          {children}  
+        </AuthContext.Provider>
     );
 };
 
